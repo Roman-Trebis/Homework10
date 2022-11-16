3363de60b28a1a051d27a400ea21a28fc0b8b944
31 <85961838+Roman-Trebis@users.noreply.github.com>
2022-11-16 21:09:45 +0300

    Initial commit

---
+++ Задание 1/Задание 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework10Exercise1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. Проверяем, является ли число палиндромом или нет
+            /*
+             * Палиндром - это число, которое одинаково выглядит в прямом и обратном направлении.
+             * Например, 121, 53235, 4884, 5555 - это числа палиндромы.
+             * Напишем программу, которая будет определять, является ли введеное пользователем число палиндромом или нет.
+			 * Алгоритм работы программы такой:
+			 * сначала она вычислит обратное число, а потом сравнит его с исходным числом. Если они одинаковые, то число является палиндромом.
+             */
+
+            long n, reversed = 0, remainder, original;
+            Console.Write("Введите любое целое число: ");
+            //n = int.Parse(Console.ReadLine());
+            n = Convert.ToInt64(Console.ReadLine());
+
+            original = n;
+
+            //вычесляем обратное число
+            while(n != 0)
+            {
+                remainder = n % 10;
+                reversed = reversed * 10 + remainder;
+                n /= 10;
+            }
+
+            //палиндром, если исходное число и обратное одинаковые
+            if (original == reversed)
+                Console.WriteLine($"{original} - палиндром");
+            else
+                Console.WriteLine($"{original} - не палиндром");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание 2/Задание 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework9Exercise1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2. Вычисляем обратное число
+            /*
+             * Вычисление обратного числа.
+             * Например, если пользователь вводит 1234,5678, 91011120 то программа возращает 4321, 8765, 2111019.
+             * Алгоритм работы программы такой:
+             * В цикле я вычисляю остаток от деления числа на 10
+             * Постепенно формируем обратное число - постепенно умножаем его на 10 и добавляем остаток от предыдущей операции деления
+            */
+
+            long n, reversed = 0, remainder;
+            Console.Write("Введите любое целое число: ");
+            //n = int.Parse(Console.ReadLine());
+            n = Convert.ToInt64(Console.ReadLine());
+
+            //вычисляем число
+            while(n != 0)
+            {
+                remainder = n % 10;
+                reversed = reversed * 10 + remainder;
+                n /= 10;
+            }
+            Console.WriteLine($"Обратное число = {reversed}");
+            Console.ReadLine();
+        }
+    }
+}
